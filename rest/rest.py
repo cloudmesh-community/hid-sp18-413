cdcad3a73585c274eed06a73e5f5f729fc6072d5
Anubhav Lavania <alavania@iu.edu>
2018-02-04 20:20:16 -0500

    First commit rest service
    	modified:   rest.py
    	new file:   settings.py

--- rest/rest.py
+++ rest/rest.py
@@ -1,1 +1,23 @@
 
+from eve import Eve
+import platform
+import psutil
+
+app = Eve()
+
+@app.route('/processor')
+def processor():
+
+    details = (platform.processor(),
+              psutil.virtual_memory(),
+              psutil.cpu_stats(),
+              psutil.virtual_memory(),
+              psutil.disk_partitions(),
+              platform.processor(),
+              platform.machine(),
+              platform.python_version(),
+              platform.system())
+    return details
+
+if __name__ == '_main_':
+    app.run()
